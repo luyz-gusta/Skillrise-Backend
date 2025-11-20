8c69e324bc8e837311d03adcd6008eb1542ee0c2
Luiz Gustavo <luizgustavo.sp2020@gmail.com>
2025-11-20 19:46:31 -0300

    feat: Banco alterado e cors atualizado

--- src/main/java/com/fiap/skillriseapi/config/CorsConfig.java
+++ src/main/java/com/fiap/skillriseapi/config/CorsConfig.java
@@ -30,7 +30,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "72.14.201.200", "http://localhost:8080", "http://localhost:8081"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://skillrise-front.vercel.app", "http://localhost:8080", "http://localhost:8081"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
